0437ffdb684d3f9ac3186b58d25372420766af24
weeger <romain.weeger@gmail.com>
2024-02-06 17:08:46 +0100

    Revert "Fix"
    This reverts commit 0d0e1c72e74ddd774d3060d622c13a32b3ca452b.

--- src/Helper/TestControllerHelper.php
+++ src/Helper/TestControllerHelper.php
@@ -45,9 +45,6 @@ class TestControllerHelper
 
         if ($checkExists) {
             if (!class_exists($controllerClass)) {
-                dump(class_exists($testControllerClass));
-                dump(is_subclass_of($testControllerClass, AbstractRoleControllerTestCase::class));
-                dd($testControllerClass);
                 throw new Exception('Unable to find controller class from '.$testControllerClass.', tried '.$controllerClass);
             }
         }
