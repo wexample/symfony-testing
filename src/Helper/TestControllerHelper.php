6ada9f56ccf560e70c01edf0318e81c9a336cf6c
weeger <romain.weeger@gmail.com>
2024-02-06 16:31:23 +0100

    Convert super classes to traits

---
+++ src/Helper/TestControllerHelper.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Wexample\SymfonyTesting\Helper;
+
+use Exception;
+use Wexample\SymfonyHelpers\Helper\ClassHelper;
+use Wexample\SymfonyHelpers\Helper\TextHelper;
+use Wexample\SymfonyHelpers\Service\Syntax\ControllerSyntaxService;
+use Wexample\SymfonyTesting\Tests\AbstractRoleControllerTestCase;
+
+class TestControllerHelper
+{
+    /**
+     * Guess controller class name from test controller class name.
+     *
+     * @param string|null $testControllerClass
+     * @throws Exception
+     */
+    public static function buildControllerClassPath(
+        string $testControllerClass = null,
+        bool $checkExists = true
+    ): string {
+        $testControllerClass = $testControllerClass ?: static::class;
+
+        if (!str_starts_with($testControllerClass, ClassHelper::NAMESPACE_SEPARATOR)) {
+            $testControllerClass = ClassHelper::NAMESPACE_SEPARATOR.$testControllerClass;
+        }
+
+        $testControllerClass = TextHelper::trimString(
+            $testControllerClass,
+            AbstractRoleControllerTestCase::APPLICATION_ROLE_TEST_CLASS_PATH,
+            ControllerSyntaxService::SUFFIX_TEST
+        );
+
+        // Count the number of chunks to remove,
+        // first separators adds one more chunk which is expected,
+        // to remove the RoleName folder.
+        $removeChunksLength = count(explode(ClassHelper::NAMESPACE_SEPARATOR, AbstractRoleControllerTestCase::APPLICATION_ROLE_TEST_CLASS_PATH));
+
+        $chunks = explode(ClassHelper::NAMESPACE_SEPARATOR, $testControllerClass);
+        $chunks = array_splice($chunks, $removeChunksLength);
+
+        array_unshift($chunks, ClassHelper::CLASS_PATH_PART_APP);
+        $controllerClass = ClassHelper::join($chunks, true);
+
+        if ($checkExists) {
+            if (!class_exists($controllerClass)) {
+                throw new Exception('Unable to find controller class from '.$testControllerClass.', tried '.$controllerClass);
+            }
+        }
+
+        return $controllerClass;
+    }
+}
--- src/Tests/AbstractRoleControllerTestCase.php
+++ src/Tests/AbstractRoleControllerTestCase.php
@@ -2,63 +2,20 @@
 
 namespace Wexample\SymfonyTesting\Tests;
 
-use Exception;
-use Wexample\SymfonyHelpers\Helper\ClassHelper;
-use Wexample\SymfonyHelpers\Helper\TextHelper;
-use Wexample\SymfonyHelpers\Service\Syntax\ControllerSyntaxService;
+use Wexample\SymfonyTesting\Helper\TestControllerHelper;
 use Wexample\SymfonyTesting\Traits\ControllerTestCaseTrait;
+use Wexample\SymfonyTesting\Traits\RoleTestCaseTrait;
 
-abstract class AbstractRoleControllerTestCase extends AbstractRoleTestCase
+abstract class AbstractRoleControllerTestCase extends AbstractSymfonyTestCase
 {
+    use RoleTestCaseTrait;
     use ControllerTestCaseTrait;
 
-    public const APPLICATION_ROLE_TEST_CLASS_PATH = '\\App\\Tests\\Application\\Roles\\';
+    public const APPLICATION_ROLE_TEST_CLASS_PATH = '\\App\\Tests\\Application\\Role\\';
 
     public static function getControllerClass(): string
     {
-        return static::buildControllerClassPath();
-    }
-
-    /**
-     * Guess controller class name from test controller class name.
-     *
-     * @param string|null $testControllerClass
-     * @throws Exception
-     */
-    public static function buildControllerClassPath(
-        string $testControllerClass = null,
-        bool $checkExists = true
-    ): string {
-        $testControllerClass = $testControllerClass ?: static::class;
-
-        if (!str_starts_with($testControllerClass, ClassHelper::NAMESPACE_SEPARATOR)) {
-            $testControllerClass = ClassHelper::NAMESPACE_SEPARATOR.$testControllerClass;
-        }
-
-        $testControllerClass = TextHelper::trimString(
-            $testControllerClass,
-            static::getRoleTestClassBasePath(),
-            ControllerSyntaxService::SUFFIX_TEST
-        );
-
-        // Count the number of chunks to remove,
-        // first separators adds one more chunk which is expected,
-        // to remove the RoleName folder.
-        $removeChunksLength = count(explode(ClassHelper::NAMESPACE_SEPARATOR, self::APPLICATION_ROLE_TEST_CLASS_PATH));
-
-        $chunks = explode(ClassHelper::NAMESPACE_SEPARATOR, $testControllerClass);
-        $chunks = array_splice($chunks, $removeChunksLength);
-
-        array_unshift($chunks, ClassHelper::CLASS_PATH_PART_APP);
-        $controllerClass = ClassHelper::join($chunks, true);
-
-        if ($checkExists) {
-            if (!class_exists($controllerClass)) {
-                throw new Exception('Unable to find controller class from '.$testControllerClass.', tried '.$controllerClass);
-            }
-        }
-
-        return $controllerClass;
+        return TestControllerHelper::buildControllerClassPath(static::class);
     }
 
     protected function setUp(): void
