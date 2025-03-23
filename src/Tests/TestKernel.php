41b00682cb1c3fc6f11424e61ab425a6892609b5
weeger <romain.weeger@gmail.com>
2025-03-23 16:10:06 +0100

    Test kernel config

--- src/Tests/TestKernel.php
+++ src/Tests/TestKernel.php
@@ -2,12 +2,16 @@
 
 namespace Wexample\SymfonyTesting\Tests;
 
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+use Wexample\SymfonyHelpers\Service\BundleService;
+use Wexample\SymfonyHelpers\Service\Entity\EntityNeutralService;
+use Wexample\SymfonyHelpers\WexampleSymfonyHelpersBundle;
 use Wexample\SymfonyTranslations\WexampleSymfonyTranslationsBundle;
 
 class TestKernel extends Kernel
@@ -18,17 +22,42 @@ class TestKernel extends Kernel
     {
         return [
             new FrameworkBundle(),
+            new DoctrineBundle(),
+            new WexampleSymfonyHelpersBundle(),
             new WexampleSymfonyTranslationsBundle(),
         ];
     }
 
-    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
+    protected function configureContainer(
+        ContainerBuilder $container,
+        LoaderInterface $loader
+    ): void
     {
         $container->loadFromExtension('framework', [
             'test' => true,
             'router' => ['utf8' => true],
             'secret' => 'test',
         ]);
+
+        $container->loadFromExtension('doctrine', [
+            'dbal' => [
+                'driver' => 'pdo_sqlite',
+                'memory' => true,
+            ],
+            'orm' => [
+                'auto_generate_proxy_classes' => true,
+                'naming_strategy' => 'doctrine.orm.naming_strategy.underscore_number_aware',
+                'auto_mapping' => true,
+            ],
+        ]);
+
+        $container->register(BundleService::class, BundleService::class)
+            ->setArguments(['@kernel'])
+            ->setPublic(true);
+
+        $container->register(EntityNeutralService::class, EntityNeutralService::class)
+            ->setArguments(['@doctrine.orm.entity_manager'])
+            ->setPublic(true);
     }
 
     protected function configureRoutes(RoutingConfigurator $routes): void
@@ -38,11 +67,11 @@ class TestKernel extends Kernel
 
     public function getCacheDir(): string
     {
-        return sys_get_temp_dir().'/cache/'.$this->environment;
+        return sys_get_temp_dir() . '/cache/' . $this->environment;
     }
 
     public function getLogDir(): string
     {
-        return sys_get_temp_dir().'/logs';
+        return sys_get_temp_dir() . '/logs';
     }
 }
